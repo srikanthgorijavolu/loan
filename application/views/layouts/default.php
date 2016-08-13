e6ac9f4693d9980defdb5c5f0d0ac48ff62faa9e
srikanth gorijavolu <gorijavolu.sris@gmail.com>
2016-08-13 18:55:15 +0530

    created lay out library for default layout

---
+++ application/views/layouts/default.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <title>Layout</title>
+    </head>
+    <body>
+        <h1>First layoput</h1>
+        <?php echo $cotent_for_layout; ?>
+    </body>
+</html>
+
+<?php $this->load->view($view,$data); ?>
